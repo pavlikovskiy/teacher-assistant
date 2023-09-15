5cddce409c3ad33ed697c352a753e1a0277f4a40
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2023-09-15 17:43:42 -0600

    feat: add jest env

---
+++ src/models/conversion.test.ts
@@ -0,0 +1,25 @@
+
+describe('temperature model test', () => {
+  test('Kelvin from Celsius', () => {
+    expect(1 + 2).toBe(3);
+  });
+  test('Rankine from Celsius', () => {
+    expect(1 + 2).toBe(3);
+  });
+  test('Fahrenheit from Celsius', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  test('Celsius from Fahrenheit', () => {
+    expect(1 + 2).toBe(3);
+  });
+})
+
+describe('volume model test', () => {
+  test('Liter from Gallon', () => {
+    expect(1 + 2).toBe(3);
+  });
+  test('Gallon from Liter', () => {
+    expect(1 + 2).toBe(3);
+  });
+})
